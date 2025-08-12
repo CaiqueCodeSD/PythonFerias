25c69dd75197aae0aeafbf6c764a3e16d692cc08
Cacá <149615073+CaiqueCodeSD@users.noreply.github.com>
2025-08-12 20:59:07 -0300

    06

---
+++ SeteProgramasPy/GeradorSenhas.py
@@ -0,0 +1,18 @@
+# Peça ao usuário o tamanho da senha e gere uma senha 
+# aleatória com letras minúsculas, maiúsculas e números.
+
+import random
+import string
+
+comprimento = int(input("Insira o tamanho da senha: "))
+
+caracEspeciais = (
+    string.ascii_uppercase +
+    string.ascii_lowercase + 
+    string.digits 
+    #string.punctuation
+)
+
+senhaAleatoria = "".join(random.choice(caracEspeciais) for _ in range(comprimento))
+
+print(f"Senha: {senhaAleatoria}")
