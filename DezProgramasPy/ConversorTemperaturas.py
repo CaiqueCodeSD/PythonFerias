c47112c6f64d0e5c7aa1fde7a3cb6445d9c9afb9
Cacá <149615073+CaiqueCodeSD@users.noreply.github.com>
2025-08-11 22:08:25 -0300

    Delete DezProgramasPy/ConversorTemperaturas.py

--- DezProgramasPy/ConversorTemperaturas.py
+++
@@ -1,7 +0,0 @@
-#Escreva um programa que leia uma temperatura em Celsius e exiba o valor convertido para Fahrenheit e Kelvin.
-
-print("=== Conversor de Temperatura ===")
-temperatura = int(input("Insira a temperatura em Celsius: "))
-print("=========")
-print(f"Temperatura em F: {(temperatura * 1.8)+32}")
-print(f"Temperatura em K: {(temperatura + 273)}")
